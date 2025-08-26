5b60b7dc307bec822bb842695fbf26ab168d788d
LiamSales <liam_miguel_sales@dlsu.edu.ph>
2025-08-26 20:42:18 +0800

    should be 90% done just some final touches, specs ambigious but tried to be faithful to it

--- dstru.kt
+++ dstru.kt
@@ -3,7 +3,7 @@ val occ = mutableListOf<Pair<Int,Int>>()
 var w = mutableListOf<Int>()
 
 var ok: Boolean = false
-var turn: Boolean = true // true: Player A, false: Player B
+var turn: Boolean = true 
 var over: Boolean = false
 var next: Boolean = false
 
@@ -14,7 +14,7 @@ var four = mutableListOf<Int>()
 var five = mutableListOf<Int>()
 var six = mutableListOf<Int>()
 
-// TODO: Implement win condition logic
+var result: String
 
 fun printBoard(s: Array<Array<Int>>) {
     for (i in s.indices) {
@@ -41,7 +41,6 @@ fun main(){
 
         val s = Array(3) { Array<Int>(3) { 0 } }
 
-        // add each coordinate pair to free 1-based indexing
         free.clear()
         occ.clear()
         for (i in 1..3) {
@@ -50,7 +49,6 @@ fun main(){
             }
         }
 
-        ok = false
         next = false
         turn = true
         over = false
@@ -62,7 +60,7 @@ fun main(){
         five.clear()
         six.clear()
 
-        var pegList = mutableListOf(1,2,3,4,5,6,7,8,9)
+        var p = mutableListOf(1,2,3,4,5,6,7,8,9)
 
         do{
 
@@ -71,19 +69,23 @@ fun main(){
 
             printBoard(s)
 
-            // TODO: Input handling for peg and pos
             var peg: Int? = null
             var pos: Pair<Int, Int>? = null
+
             while (peg == null) {
-                println("Enter peg (1-9):")
-                peg = readlnOrNull()?.toIntOrNull()?.takeIf { it in pegList }
+                println("Choose peg (1-9):")
+                peg = readlnOrNull()?.toIntOrNull()?.takeIf { it in p }
                 if (peg == null) println("Invalid peg. Try again.")
             }
+
+            p.remove(peg)
+
             while (pos == null) {
                 println("Enter row (1-3):")
                 val row = readlnOrNull()?.toIntOrNull()?.takeIf { it in 1..3 }
                 println("Enter column (1-3):")
                 val col = readlnOrNull()?.toIntOrNull()?.takeIf { it in 1..3 }
+                
                 if (row != null && col != null && free.contains(Pair(row, col))) {
                     pos = Pair(row, col)
                 } else {
@@ -93,14 +95,7 @@ fun main(){
 
             nextPlayerMove(peg, pos)
 
-            // TODO: Implement win condition check here
-            // if (checkWin()) {
-            //     over = true
-            //     gameOver(over)
-            // }
-
-            // Alternate player only after valid move
-            turn = !turn
+            s[pos.first - 1][pos.second - 1] = peg
 
         } while(!over)
 
@@ -121,39 +116,41 @@ fun main(){
 
 fun nextPlayerMove(peg: Int, pos: Pair<Int,Int>){
 
-    if (free.contains(pos)){
-        ok = true
-        free.remove(pos)
-        occ.add(pos)
-    } else {
-        ok = false
-        println("Position already occupied.")
-        return
+    free.remove(pos)
+    occ.add(pos)
+    ok = !ok
+
+    if (ok){
+        when (pos.first){
+            1 -> one.add(peg)
+            2 -> two.add(peg)
+            3 -> three.add(peg)
+        }
+
+        when (pos.second){
+            1 -> four.add(peg)
+            2 -> five.add(peg)
+            3 -> six.add(peg)
+        }
+        next = !next
     }
 
-    when (pos.first){
-        1 -> one.add(peg)
-        2 -> two.add(peg)
-        3 -> three.add(peg)
-    }
+    w.add(peg)
 
-    when (pos.second){
-        1 -> four.add(peg)
-        2 -> five.add(peg)
-        3 -> six.add(peg)
-    }
+    turn = !turn
 
-    // TODO: Update board state (s) with peg value
-
-    next = !next
 }
 
-fun gameOver(over: Boolean): String{
-    // TODO: Implement proper win condition check
-    if (over /* && win condition */)
-        return "Player A wins"
-    else
-        return "Player B wins"
-}
+fun gameOver(over: Boolean) {
 
-// TODO: Implement checkWin() function to determine if the game
+    var sum = 0
+    w.forEach { a -> sum+=a }
+
+    if (over && turn && w.size==3 && sum<=15)
+        result = "Player B wins"
+    else if (over && !turn && w.size==3 && sum<=15)
+        result = "Player A wins"
+    else if (!over) turn =!turn
+    else 
+        result = "Game ends in a draw"
+}
